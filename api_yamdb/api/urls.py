e0f0d182aeb7e75df00dcc0f4b42515b22e8b987
Иван Максимов <ivanmaksimov02@mail.ru>
2022-03-12 09:20:34 +0300

    part_a

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,6 +9,6 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/mail/', get_confirmation_code),
+    path('v1/auth/signup/', get_confirmation_code),
     path('v1/auth/token/', get_jwt_token)
 ]
