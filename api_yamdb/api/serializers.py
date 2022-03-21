3bbfec03f1c07f1ab1e57a3bd7703412382e8fc9
Pasha Zakharov <zaxarow.paxa@yandex.ru>
2022-03-21 14:31:24 +0500

    pep 8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from django.shortcuts import get_object_or_404
 
 from reviews.models import (User, Review, Comment,
                             Category, Genre, Title)
